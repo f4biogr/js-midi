e838aa2dea601752b8b6c51931a27e9b3272368a
Fábio da Rosa <fabio.gorosa@gmail.com>
2023-03-07 20:23:02 -0300

    :beers: conclusão do curso

--- main.js
+++ main.js
@@ -1,5 +1,30 @@
-function tocaSomPom() {
-  document.querySelector("#som_tecla_pom").play()
+function tocaSom(idAudio) {
+  const audioElement = document.querySelector(idAudio)
+
+  if (audioElement && audioElement.localName === "audio") {
+    audioElement.play()
+  } else {
+    console.log("Elemento não encontrado.")
+  }
 }
 
-document.querySelector(".tecla_pom").addEventListener("click", tocaSomPom)
+const listaDeTeclas = document.querySelectorAll(".tecla")
+
+for (let i = 0; i < listaDeTeclas.length; i++) {
+  const tecla = listaDeTeclas[i]
+  const instrumento = tecla.classList[1]
+
+  tecla.onclick = function () {
+    tocaSom(`#som_${instrumento}`)
+  }
+
+  tecla.onkeydown = function (event) {
+    if (event.code === "Space" || event.code === "Enter") {
+      tecla.classList.add("ativa")
+    }
+  }
+
+  tecla.onkeyup = function () {
+    tecla.classList.remove("ativa")
+  }
+}
